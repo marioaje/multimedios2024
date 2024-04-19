b42ff2091fd9056b3229901b8fc3bfe55c8bd412
marioaje <marioaje@gmail.com>
2024-04-19 14:59:55 -0600

    19 Abril

--- Semana 5/Proyecto JS 01/js/usuarios.js
+++ Semana 5/Proyecto JS 01/js/usuarios.js
@@ -113,7 +113,7 @@ function mostrarDatos(datos){
     
     if(datos.code == 200){
         for (const iterator of datos.data) {  
-        
+        //onclick="editar('${iterator.id}', '${iterator.name}', '${iterator.password}', '${iterator.email}')"
             datosTabla.innerHTML += `
                 <tr class="">
                     <td>
@@ -121,7 +121,7 @@ function mostrarDatos(datos){
                         name=""
                         id=""
                         class="btn btn-success"
-                        onclick="editar('${iterator.id}', '${iterator.name}', '${iterator.password}', '${iterator.email}')"
+                        onclick="editar('${encodeURIComponent(JSON.stringify(iterator))}')"
                         role="button"
                         ><svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-edit"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M7 7h-1a2 2 0 0 0 -2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2 -2v-1" /><path d="M20.385 6.585a2.1 2.1 0 0 0 -2.97 -2.97l-8.415 8.385v3h3l8.385 -8.415z" /><path d="M16 5l3 3" /></svg></a
                     >                    
@@ -153,11 +153,15 @@ function loadspinner(){
     document.getElementById("spinnerload").innerHTML = spinner;
 }
 //onclick="editar('${iterator.id}', '${iterator.name}', '${iterator.password}', '${iterator.email}')"
-function editar(id, name, password, email) {
-    console.log(id);
-    console.log(name);
-    console.log(password);
-    console.log(email);
+//function editar(id, name, password, email) {
+function editar(objeto) {
+    console.log(objeto);
+    objeto = JSON.parse(decodeURIComponent(objeto));
+    console.log(objeto);
+    // console.log(id);
+    // console.log(name);
+    // console.log(password);
+    // console.log(email);
    //no me sirvio console.log( JSON.stringify(objeto));
    //console.log( JSON.parse(objeto));
 }
